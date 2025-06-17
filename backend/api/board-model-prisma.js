6ef9d6e6544cf17c6aebb2d31ff17da46267ea40
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-17 16:56:43 -0700

    Added routing for board id

--- backend/api/board-model-prisma.js
+++ backend/api/board-model-prisma.js
@@ -4,5 +4,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 module.exports = {
-    
+    async find(where) {
+        // SELECT * FROM "Board";
+        const boards = await prisma.board.findMany({ where });
+        return boards;
+    },
+
+    async findById(id) {
+        // SELECT * FROM "Board" WHERE id = 1;
+        const board = await prisma.board.findUnique({where: { id }})
+        return board;
+    },
 }
--- backend/api/server.js
+++ backend/api/server.js
@@ -9,8 +9,33 @@ server.use(cors())
 server.use(helmet())
 
 // [GET] /api/boards
-server.get('/api/boards', (req,res,next) => {
+server.get('/api/boards', async (req,res,next) => {
+    const query = req.query;
+    try{
+        const boards = await Board.find(query)
+        if(boards.length){
+            res.json(boards);
+        }else {
+            next({ status: 404, message: 'No boards found match the search criteria' })
+        }
+    } catch (err) {
+    next(err)
+    }
+})
 
+// [GET] /api/boards/:id
+server.get('/api/boards/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+    try {
+        const board = await Board.findById(id);
+        if(board) {
+            res.json(board);
+        }else {
+            next({ status: 404, message: 'No board found match the id' })
+        }
+    }catch (err){
+        next(err)
+    }
 })
 
 server.use((req, res, next) => {
